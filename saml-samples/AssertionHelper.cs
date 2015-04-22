d584d6e259696c65e605ab1a2256517e292e08cd
Steve Patches <steve@careevolution.com>
2015-04-22 11:50:10 -0400

    WIF signed assertion creation

---
+++ saml-samples/AssertionHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+
+namespace saml_samples
+{
+	class AssertionHelper
+	{
+		public AssertionHelper( X509Certificate2 signingCert ) 
+			: this(signingCert, null)
+		{
+		}
+
+		public AssertionHelper( X509Certificate2 signingCert, X509Certificate2 encryptionCert )
+		{
+			SigningCredentials = new X509SigningCredentials( signingCert, SecurityAlgorithms.RsaSha1Signature, SecurityAlgorithms.Sha1Digest );
+			if ( encryptionCert != null )
+			{
+				EncryptionCredentials = new X509EncryptingCredentials( encryptionCert );
+			}
+		}
+
+		public string SignAssertion( Saml2Assertion assertion )
+		{
+			string signedAssertion = String.Empty;
+			var token = new Saml2SecurityToken( assertion );
+			var handler = new Saml2SecurityTokenHandler();
+			assertion.SigningCredentials = SigningCredentials;
+			assertion.EncryptingCredentials = EncryptionCredentials;
+			using ( var stringWriter = new StringWriter() )
+			{
+				using ( var xmlWriter = XmlWriter.Create( stringWriter,
+				new XmlWriterSettings { OmitXmlDeclaration = true } ) )
+				{
+					handler.WriteToken( xmlWriter, token );
+				}
+				signedAssertion = stringWriter.ToString();
+			}
+			return signedAssertion;
+		}
+
+		private readonly SigningCredentials SigningCredentials;
+		private readonly EncryptingCredentials EncryptionCredentials;
+	}
+}
---
+++ saml-samples/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace saml_samples
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			var response =
+			@"<saml2p:Response xmlns:saml2p=""urn:oasis:names:tc:SAML:2.0:protocol""
+            xmlns:saml2=""urn:oasis:names:tc:SAML:2.0:assertion""
+            ID = ""_{0}"" Version=""2.0"" IssueInstant=""2015-01-01T00:00:00Z"">
+                <saml2:Issuer>https://idp.example.com</saml2:Issuer>
+                <saml2p:Status>
+                    <saml2p:StatusCode Value=""urn:oasis:names:tc:SAML:2.0:status:Success"" />
+                </saml2p:Status>
+                {1}
+            </saml2p:Response>";
+
+			var assertion = new Saml2Assertion( new Saml2NameIdentifier( "https://idp.example.com" ) );
+			assertion.Subject = new Saml2Subject( new Saml2NameIdentifier( "SomeUser" ) );
+			assertion.Subject.SubjectConfirmations.Add( new Saml2SubjectConfirmation( new Uri( "urn:oasis:names:tc:SAML:2.0:cm:bearer" ) ) );
+
+			assertion.Conditions = new Saml2Conditions {
+				NotOnOrAfter = new DateTime( 2100, 1, 1 ) 
+			};
+
+			assertion.Statements.Add( new Saml2AttributeStatement( new Saml2Attribute( "FooID", "12345" ) ) );
+
+			var signingCert = new X509Certificate2( @"C:\Dev\STS.pfx", "somepassword" );
+			var assertionHelper = new AssertionHelper( signingCert );
+			var encryptedAssertion = assertionHelper.SignAssertion( assertion );
+			var fullResponse = string.Format( response, Guid.NewGuid().ToString(), encryptedAssertion );
+
+			File.WriteAllText( @"C:\dev\samlResponse.xml", fullResponse );
+		}
+	}
+}
